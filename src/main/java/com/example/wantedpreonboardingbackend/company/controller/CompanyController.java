488626fb208a089af02174600609692b9b9c8368
12hyeon <hyeonjeong1212@naver.com>
2023-10-08 15:36:18 +0900

    feat: swagger 설정

--- src/main/java/com/example/wantedpreonboardingbackend/company/controller/CompanyController.java
+++ src/main/java/com/example/wantedpreonboardingbackend/company/controller/CompanyController.java
@@ -1,18 +1,33 @@
 package com.example.wantedpreonboardingbackend.company.controller;
 
+import com.example.wantedpreonboardingbackend.company.response.CompanyResponse;
 import com.example.wantedpreonboardingbackend.company.service.CompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/v1")
 @RestController
 @RequiredArgsConstructor
 public class CompanyController {
 
     private final CompanyService companyService;
 
-    @PostMapping("/companies")
+    @Operation(summary = "회사 등록 요청", description = "회사 정보가 추가됩니다.", tags = { "Company Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = CompanyResponse.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
+    @PostMapping("v1/companies")
     public Object saveCompany(@RequestParam String name, @RequestParam long number) {
         return new ResponseEntity<>(companyService.saveCompany(name, number), HttpStatus.OK);
     }
---
+++ src/main/java/com/example/wantedpreonboardingbackend/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.wantedpreonboardingbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Job Posting App",
+                description = "Job Posting app api 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Job Posting API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
