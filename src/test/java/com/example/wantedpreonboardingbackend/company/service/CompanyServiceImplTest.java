704b1f223d4cbe76b425e6c84ef2ea6c09c67b3c
12hyeon <hyeonjeong1212@naver.com>
2023-10-12 16:26:47 +0900

    test: CompanyService 수정

--- src/test/java/com/example/wantedpreonboardingbackend/company/service/CompanyServiceImplTest.java
+++ src/test/java/com/example/wantedpreonboardingbackend/company/service/CompanyServiceImplTest.java
@@ -4,6 +4,7 @@ import com.example.wantedpreonboardingbackend.common.BaseResponse;
 import com.example.wantedpreonboardingbackend.company.domain.Company;
 import com.example.wantedpreonboardingbackend.company.dto.CompanyDto;
 import com.example.wantedpreonboardingbackend.company.repository.CompanyRepository;
+import com.example.wantedpreonboardingbackend.exception.CustomException;
 import com.example.wantedpreonboardingbackend.exception.ExceptionCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,29 +32,31 @@ public class CompanyServiceImplTest {
     @DisplayName("중복된 번호 시 예외 발생 - 회사 저장")
     public void testSaveCompany() {
         // Given
-        int companyNumber = 456;
-        Mockito.when(companyRepository.findByNumber(companyNumber)).thenReturn(Optional.empty());
-        Mockito.when(companyRepository.save(Mockito.any(Company.class))).thenReturn(new Company("New Company", companyNumber));
+        int companyNumber = 123;
+        Company existingCompany = new Company("Existing Company", companyNumber);
+        Mockito.when(companyRepository.findByNumber(companyNumber)).thenReturn(Optional.of(existingCompany));
 
-        // When
-        BaseResponse response = companyService.saveCompany("New Company", companyNumber);
-
-        // Then
-        assertEquals(ExceptionCode.SAVE_COMPANY_OK.getCode(), response.getCode());
+        // When & Then
+        CustomException exception = assertThrows(CustomException.class,
+                () -> companyService.saveCompany("New Company", companyNumber)
+        );
+        assertEquals(ExceptionCode.DUPLICATE_COMPANY_NUMBER, exception.getExceptionCode());
     }
 
     @Test
     @DisplayName("중복되지 않은 번호 - 회사 저장")
     public void testSaveCompany2() {
         // Given
-        int companyNumber = 123;
-        Company existingCompany = new Company("Existing Company", companyNumber);
-        Mockito.when(companyRepository.findByNumber(companyNumber)).thenReturn(Optional.of(existingCompany));
+        int companyNumber = 456;
+        Company newCompany = new Company("Existing Company", companyNumber);
+        Mockito.when(companyRepository.findByNumber(companyNumber)).thenReturn(Optional.empty());
+        Mockito.when(companyRepository.save(Mockito.any(Company.class))).thenReturn(newCompany); // company - id 설정
 
         // When
-        BaseResponse response = companyService.saveCompany("New Company", companyNumber);
+        BaseResponse<CompanyDto.CompanyResponse> response = companyService.saveCompany("New Company", companyNumber);
 
         // Then
-        assertEquals(ExceptionCode.DUPLICATE_COMPANY_NUMBER.getCode(), response.getCode());
+        assertEquals(ExceptionCode.SAVE_COMPANY_OK.getCode(), response.getCode());
+        assertNotNull(response.getResult().getCompanyId());
     }
 }
